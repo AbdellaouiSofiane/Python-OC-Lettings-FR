6c78c5b5fb951dc298531ebefee8e0f3648ddcc6
sofiane <abdellaoui.sofiane.esb@gmail.com>
2022-10-24 17:19:08 +0200

    rename and move template html files

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -18,7 +18,7 @@ def index(request):
 def lettings_index(request):
     lettings_list = Letting.objects.all()
     context = {'lettings_list': lettings_list}
-    return render(request, 'lettings_index.html', context)
+    return render(request, 'lettings/index.html', context)
 
 
 # Cras ultricies dignissim purus, vitae hendrerit ex varius non. In accumsan porta nisl id
@@ -36,7 +36,7 @@ def letting(request, letting_id):
         'title': letting.title,
         'address': letting.address,
     }
-    return render(request, 'letting.html', context)
+    return render(request, 'lettings/letting.html', context)
 
 
 # Sed placerat quam in pulvinar commodo. Nullam laoreet consectetur ex, sed consequat libero
@@ -45,7 +45,7 @@ def letting(request, letting_id):
 def profiles_index(request):
     profiles_list = Profile.objects.all()
     context = {'profiles_list': profiles_list}
-    return render(request, 'profiles_index.html', context)
+    return render(request, 'profiles/index.html', context)
 
 
 # Aliquam sed metus eget nisi tincidunt ornare accumsan eget lac
@@ -55,4 +55,4 @@ def profiles_index(request):
 def profile(request, username):
     profile = Profile.objects.get(user__username=username)
     context = {'profile': profile}
-    return render(request, 'profile.html', context)
+    return render(request, 'profiles/profile.html', context)
